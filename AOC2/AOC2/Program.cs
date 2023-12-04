d9988cf1021a493d6e942db8d9181d318553153c
Lukewal99 <Lukewal99@gmx.com>
2023-12-04 18:22:07 +0000

    Day 2 And 4

---
+++ AOC2/AOC2/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+StreamReader sr = new StreamReader("C:\\Users\\lukew\\Desktop\\AoC23\\Advent.csv");
+string[] data = sr.ReadToEnd().Split("\r\n");
+Dictionary<string,int> maximums = new Dictionary<string,int>();
+maximums.Add("red", 0);
+maximums.Add("green", 0);
+maximums.Add("blue", 0);
+int IDSum = 0;
+int task = 2;
+int powerSum = 0;
+
+for (int i = 0; i < data.Length; i++)
+{
+	string line = data[i];
+
+    string[] game = line.Split(": ")[1].Split("; ");
+	foreach (string pull in game)
+	{
+		string[] colours = pull.Split(", ");
+		foreach (string colour in colours)
+		{
+			string[] colourSplit = colour.Split(" ");
+			int temp1 = Convert.ToInt32(colourSplit[0]);
+			int temp2 = maximums[colourSplit[1]];
+            if (Convert.ToInt32(colourSplit[0]) > maximums[colourSplit[1]])
+			{
+				maximums[colourSplit[1]] = Convert.ToInt32(colourSplit[0]);
+
+            }
+		}
+	}
+
+	if (task == 1)
+	{
+		if (maximums["red"] <= 12 && maximums["green"] <= 13 && maximums["blue"] <= 14)
+		{
+			IDSum += i + 1;
+		}
+	}
+	else if(task == 2)
+	{
+		powerSum += maximums["red"] * maximums["green"] * maximums["blue"];
+
+    }
+	maximums["red"] = 0;
+    maximums["green"] = 0;
+    maximums["blue"] = 0;
+}
+
+if (task == 1)
+{
+	Console.WriteLine(IDSum);
+}
+if (task == 2)
+{
+	Console.WriteLine(powerSum);
+}
---
+++ AOC4/AOC4/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
+int task = 2;
+
+StreamReader sr = new StreamReader("C:\\Users\\lukew\\Desktop\\AoC23\\Advent.csv");
+List<string> data = sr.ReadToEnd().Split("\r\n").ToList();
+int numsMatching = 0;
+double scoreSum = 0;
+
+
+if (task == 1)
+{
+    foreach (string line in data)
+    {
+        string[] Ticket = line.Split(":")[1].Split(" |");
+        string[] WinningNumbers = Ticket[0].Replace("  ", " ").Split(" ");
+        string[] NumbersYouHave = Ticket[1].Replace("  ", " ").Split(" ");
+        numsMatching = 0;
+        foreach (string number in NumbersYouHave)
+        {
+            if (WinningNumbers.Contains(number) && number != "")
+            {
+                numsMatching++;
+            }
+        }
+
+        if (numsMatching > 0)
+        {
+            scoreSum += Math.Pow(2, (numsMatching - 1));
+        }
+    }
+    Console.WriteLine(scoreSum);
+}
+else if(task == 2)
+{
+    // All of the cards, and how many matching numbers they have
+    // Card Number / Numbers that match
+    Dictionary<int, int> reference = new Dictionary<int, int>();
+    foreach (string line in data)
+    {
+        numsMatching = 0;
+        string[] Ticket = line.Split(": ");
+        int TicketNumber = Convert.ToInt32(Ticket[0].Split(" ").Last()); 
+        
+        string[] Numbers = Ticket[1].Split(" | ");
+        string[] WinningNumbers = Numbers[0].Split(" ");
+        string[] NumbersYouHave = Numbers[1].Split(" ");
+
+        foreach (string number in NumbersYouHave)
+        {
+            if (WinningNumbers.Contains(number) && number != "")
+            {
+                numsMatching++;
+            }
+        }
+
+        reference.Add(TicketNumber, numsMatching);
+
+    }
+   
+    // Card Number / How many of that card I have
+    Dictionary<int,int> cardsCounts = new Dictionary<int, int>();
+    foreach (KeyValuePair<int,int> card in reference)
+    {
+        cardsCounts.Add(card.Key, 1);
+    }
+
+    foreach(KeyValuePair<int,int> card in cardsCounts)
+    {
+        for (int i = 1; i <= reference[card.Key]; i++)
+        {
+            cardsCounts[card.Key+i] += card.Value;
+        }
+    }
+
+    Console.WriteLine(cardsCounts.Values.Sum());
+}
